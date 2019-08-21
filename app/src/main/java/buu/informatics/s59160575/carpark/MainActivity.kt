9e4107f613350490a9c1eb1bbc3b68579438b815
Armakuji <draculalovekill@hotmail.com>
2019-08-21 18:20:54 +0700

    add UI

---
+++ app/src/main/java/buu/informatics/s59160575/carpark/MainActivity.kt
@@ -0,0 +1,12 @@
+package buu.informatics.s59160575.carpark
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
