4f150b81600aa743b1b848648b2e17e42313c34a
Armakuji <draculalovekill@hotmail.com>
2019-08-21 18:50:12 +0700

    start code

--- app/src/main/java/buu/informatics/s59160575/carpark/MainActivity.kt
+++ app/src/main/java/buu/informatics/s59160575/carpark/MainActivity.kt
@@ -2,11 +2,20 @@ package buu.informatics.s59160575.carpark
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import androidx.databinding.DataBindingUtil
+import buu.informatics.s59160575.carpark.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+
+    private fun hindOnStart(view: View) {
+        val carText = findViewById<EditText>(R.id.id_car)
+
     }
 }
