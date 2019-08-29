a544967e8254b9a68b3a9e4c5d0e515f9f3fa666
Armakuji <draculalovekill@hotmail.com>
2019-08-29 16:56:46 +0700

    add navcontroller back button bar

--- app/src/main/java/buu/informatics/s59160575/carpark/MainActivity.kt
+++ app/src/main/java/buu/informatics/s59160575/carpark/MainActivity.kt
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import buu.informatics.s59160575.carpark.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -14,7 +17,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val navController = this.findNavController(R.id.myNavHostFragment)
 
+        NavigationUI.setupActionBarWithNavController(this ,navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+
+        return navController.navigateUp()
     }
 
 }
